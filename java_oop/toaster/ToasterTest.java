a6d59f49242997c9c4b7eeaa7713c433bd3f53ee
Nathan Prater <nprater86@gmail.com>
2022-08-17 11:51:55 -0500

    added a couple more tests

--- java_oop/toaster/ToasterTest.java
+++ java_oop/toaster/ToasterTest.java
@@ -5,8 +5,8 @@ public class ToasterTest {
         Toaster fourToaster = new Toaster(4);
 
         //test the getCapacity method
-        System.out.println(twoToaster.getCapacity());
-        System.out.println(fourToaster.getCapacity());
+        System.out.println(twoToaster.getCapacity()); //output: 2
+        System.out.println(fourToaster.getCapacity()); //output: 4
 
         //add some bread to the bread List
         Bread bread1 = new Bread();
@@ -14,19 +14,23 @@ public class ToasterTest {
         Bread bread3 = new Bread();
         twoToaster.addBread(bread1);
         twoToaster.addBread(bread2);
-        twoToaster.addBread(bread3);
+        twoToaster.addBread(bread3); //should print that the toaster is full
 
         //test breadStatus method
-        twoToaster.breadStatus();
-        fourToaster.breadStatus();
+        twoToaster.breadStatus(); //output: all bread is UNTOASTED
+        fourToaster.breadStatus(); //output: toaster is empty since we never added bread to this one
 
         //test toastBread method
-        twoToaster.toastBread();
-        twoToaster.breadStatus();
+        twoToaster.toastBread(); //toasts all the bread
+        twoToaster.breadStatus(); //should print out that all bread is TOASTED state now
 
 
         //test emptyToaster
-        twoToaster.emptyToaster();
-        twoToaster.breadStatus();
+        twoToaster.emptyToaster(); //will clear the array list
+        twoToaster.breadStatus(); //should show that toaster is empty now
+
+        //check bread1 and bread2 status after being in toaster
+        System.out.println(bread1.getState());
+        System.out.println(bread2.getState());
     }
 }
